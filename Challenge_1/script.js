873b53ac86590e9100bc6300fe3a673d5b757797
Zakariae Abassi <Zakariaeabassioff@gmail.com>
2025-01-04 21:15:34 +0100

    First challenge completed.

---
+++ Challenge_1/script.js
@@ -0,0 +1,16 @@
+const eyePath = document.querySelector(".eye");
+const passwordInput = document.getElementById("password");
+
+const togglePasword = () => {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordInput.classList.toggle('active');
+        eyePath.classList.toggle('active');
+    } else {
+        passwordInput.type = "password";
+        passwordInput.classList.toggle('active');
+        eyePath.classList.toggle('active');
+    }
+}
+
+eyePath.addEventListener("click", togglePasword);
